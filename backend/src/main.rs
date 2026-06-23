9028d44378584d59a4281a5707dc2733445b90d4
anagha0407 <anaghabalaji0407@gmail.com>
2026-06-23 19:39:17 +0530

    Connect lost and found forms to backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,78 +1,61 @@
+use actix_web::{post, get, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 use actix_cors::Cors;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize)]
-struct LostItem {
-    item_name: String,
-    description: String,
-    location: String,
-}
-
-#[derive(Deserialize)]
-struct FoundItem {
-    item_name: String,
-    description: String,
-    location: String,
-}
-
-#[derive(Serialize)]
-struct ApiResponse {
-    message: String,
-}
 
 #[get("/api/health")]
 async fn health() -> impl Responder {
-    HttpResponse::Ok().json(serde_json::json!({
-        "status": "Backend is working"
-    }))
+    HttpResponse::Ok().json(
+        serde_json::json!({
+            "status": "Backend is working"
+        })
+    )
 }
 
-#[post("/api/lost-items")]
-async fn create_lost_item(item: web::Json<LostItem>) -> impl Responder {
-    println!(
-        "Lost Item: {} | {} | {}",
-        item.item_name,
-        item.description,
-        item.location
-    );
 
-    HttpResponse::Ok().json(ApiResponse {
-        message: "Lost item received".to_string(),
-    })
+#[derive(Deserialize)]
+struct Report {
+    item_name: String,
+    description: String,
+    location: String,
+    date: String,
+    report_type: String,
 }
 
-#[post("/api/found-items")]
-async fn create_found_item(item: web::Json<FoundItem>) -> impl Responder {
-    println!(
-        "Found Item: {} | {} | {}",
-        item.item_name,
-        item.description,
-        item.location
-    );
 
-    HttpResponse::Ok().json(ApiResponse {
-        message: "Found item received".to_string(),
-    })
+#[post("/api/reports")]
+async fn create_report(
+    report: web::Json<Report>
+) -> impl Responder {
+
+    println!("Received report:");
+    println!("{}", report.item_name);
+
+    HttpResponse::Ok().json(
+        serde_json::json!({
+            "message": "Report received successfully"
+        })
+    )
 }
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
     println!("Server running on http://127.0.0.1:3001");
 
     HttpServer::new(|| {
-        let cors = Cors::default()
-            .allow_any_origin()
-            .allow_any_method()
-            .allow_any_header();
 
-        App::new()
-            .wrap(cors)
-            .service(health)
-            .service(create_lost_item)
-            .service(create_found_item)
+    let cors = Cors::default()
+        .allow_any_origin()
+        .allow_any_method()
+        .allow_any_header();
+
+    App::new()
+        .wrap(cors)
+        .service(health)
+        .service(create_report)
     })
-    .bind(("127.0.0.1", 3001))?
+    .bind(("127.0.0.1",3001))?
     .run()
     .await
 }
